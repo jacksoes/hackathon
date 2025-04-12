91d5f138b11c095a3d76b3bbc155b8f2ec790342
jack soesman <jsoesman1@yahoo.com>
2025-04-12 01:16:24 -0400

    add agents

---
+++ backend/agents.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+import dspy
+
+data_set = "./Crime_Data_from_2020_to_Present.csv"
--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-import dspy
+
 
  
 
