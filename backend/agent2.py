09e71ee41f9d73d0959a831c22ccda247f0373dc
Jasmeen Kaur <jasmeenkaur@Jasmeens-MacBook-Pro.local>
2025-04-12 02:40:56 -0400

    add preprocessor agent

--- backend/agent2.py
+++ backend/agent2.py
@@ -1,1 +1,8 @@
 import dspy
+class data_preprocessing_agent(dspy.Signature):
+    # Called to refine the query incase user query not elaborate
+    """It is your job to take data and restructure it according to user defined input"""
+    dataset = dspy.InputField(desc="Data based on crime in the format of a .csv file.")
+    Agent_desc = dspy.InputField(desc= "It is your job to restructure the data as the user requests")
+    goal = dspy.InputField(desc="Eliminate all other data that doesn't include time or type of weapon")
+    refined_goal = dspy.OutputField(desc='It needs to be in a .csv file and it need to originate from .csv input and it need to have two columns named Weapon Used Cd and another that says TIME OCC')
